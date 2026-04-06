64aeaf76ec12c19de25dfd452abdf773074b5c6b
MeesterMarcus <lorenzanamarcus@gmail.com>
2026-04-06 16:04:21 -0500

    rdr

--- functions/t/[[slug]].js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  const { request, env } = context;
-  const url = new URL(request.url);
-  const assetUrl = new URL("/t/index.html", url);
-
-  return env.ASSETS.fetch(new Request(assetUrl, request));
-}
